769f696f35ee949beeb79ff404888325826d6431
Jkilker <116875574+Jkilker@users.noreply.github.com>
2022-11-22 12:56:21 +0000

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,53 @@
+var mySong, amplitude, play;
+
+function preload(){
+mySong = loadSound("assets/massiveAttackAngel.mp3")
+}
+
+function setup() {
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
+    canvas.parent("p5container");
+    play = false;
+    rectMode(CENTER);
+    background(0);
+
+    var button = createButton("Play");
+    button.center();
+    button.addClass("btn");
+
+    button.mousePressed(function(){
+        play = true;
+        mySong.play();
+        button.hide();
+    })
+
+   
+
+    noStroke();
+
+    amplitude = new p5.Amplitude();
+}
+
+function draw() {
+    if(play == true){
+        var level = amplitude.getLevel();
+        console.log(level);
+    
+        var vol = map(level, 0, 1, 0, 1000)
+        var col = map(level, 0, 1, 0, 255)
+    
+        background(col);
+    
+        if(vol > 200){
+            fill(col + 10)
+            ellipse(width/2, height/2, vol + 100);
+        }
+    
+        fill(col, 255, 255);
+        ellipse(width/2, height/2, vol);
+    
+        fill(255, col, 255);
+        ellipse(width/2, height/2, vol/2);
+    }
+    
+}
